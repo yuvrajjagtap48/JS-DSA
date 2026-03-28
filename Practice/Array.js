a20ce17b8892479c64aebfd92c732c03351a2501
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2026-03-28 14:18:46 +0530

    28-03-2026

--- Practice/Array.js
+++ Practice/Array.js
@@ -1,23 +1,41 @@
-function joinTwoArray(arr1, arr2, separator) {
-    let result = '';
+// function joinTwoArray(arr1, arr2, separator) {
+//     let result = '';
+//     for(let i = 0; i < arr1.length; i++){
+//         result = result + arr1[i];
+//         if(i !== arr1.length - 1){
+//             result = result + separator;
+//         }
+//     }
+//     for(let i = 0; i < arr2.length; i++){
+//         result = result + arr2[i];
+//         if(i !== arr2.length - 1){
+//             result = result + separator;
+//         }
+//     }
+//     return result;
+// }
+
+
+// // example usage
+// const arr1 = ['Hello', 'World'];
+// const arr2 = [  'JavaScript'];
+// const separator = ' ';
+// console.log(joinTwoArray(arr1, arr2 , separator)); // Output: "Hello World JavaScript"
+
+
+function joinTwoArray(arr1, arr2) {
+    let result = [];
     for(let i = 0; i < arr1.length; i++){
-        result = result + arr1[i];
-        if(i !== arr1.length - 1){
-            result = result + separator;
-        }
+        result.push(arr1[i]);
     }
     for(let i = 0; i < arr2.length; i++){
-        result = result + arr2[i];
-        if(i !== arr2.length - 1){
-            result = result + separator;
-        }
+        result.push(arr2[i]);
     }
     return result;
 }
 
-
 // example usage
 const arr1 = ['Hello', 'World'];
 const arr2 = [  'JavaScript'];
-const separator = ' ';
-console.log(joinTwoArray(arr1, arr2 , separator)); // Output: "Hello World JavaScript"
+console.log(joinTwoArray(arr1, arr2)); // Output: ["Hello", "World", "JavaScript"]
+
