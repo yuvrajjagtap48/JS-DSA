6348890f4d3fe9929e7b3eda1b72d66288987d59
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2026-04-16 22:28:02 +0530

    16-04-2026

--- Practice/LeetCodePractice/BinarySearch/FirstBadVersion.js
+++ Practice/LeetCodePractice/BinarySearch/FirstBadVersion.js
@@ -19,9 +19,3 @@ console.log(firstBadVersion(n));
 function isBadVersion(n) {
     return n >= 4;
 } 
-function firstBadVersion(n){
-    let l = 0;
-    let r = n;
-    while(l < r){
-        let mid = Math.floor(l + (r - l) / 2);
-        if(!isBadVersion(mid)){
