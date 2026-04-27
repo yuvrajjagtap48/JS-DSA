d417e662a41cc8494ae24b377841532c93386aaf
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2026-04-27 14:01:12 +0530

    27-04-2026

--- LinkedList/MiddleOfTheLinkedList.js
+++ LinkedList/MiddleOfTheLinkedList.js
@@ -23,7 +23,15 @@ function middleNode(head) {
 //     return slow;
 // }
 
-
+function middleNode(head) {
+    let slow = head;
+    let fast = head;
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+}
 
 
 function ListNode(val, next = null) {
