2a707181ed98b7099ecb76e3ca1a215f2d7d4f19
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2026-05-02 11:30:06 +0530

    02-05-2026

--- LeetCode/IntegerToRoman.js
+++ LeetCode/IntegerToRoman.js
@@ -13,3 +13,16 @@ function InttoRoman(num){
 
 const num = 14;
 console.log(InttoRoman(num));
+
+function InttoRoman(num){
+    const v = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const s = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    let res = "";
+    for(let i = 0; i < v.length; i++){
+        while(num >= v[i]){
+            res = res + s[i];
+            num = num - v[i];
+        }
+    }   
+    return res;
+}
