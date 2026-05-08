fb01b60e84d6c9c35e6fc4d3af1f7195ceeea5e4
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2026-05-08 14:18:19 +0530

    08-05-2026

--- Practice/LeetCodePractice/Array/ClimbingStairs.js
+++ Practice/LeetCodePractice/Array/ClimbingStairs.js
@@ -14,3 +14,17 @@ function climbing(n){
 
 let result = climbing(5);
 console.log(result);
+
+
+function climbingStairs(n) {   
+    if (n <= 2) return n;
+
+    let a = 1;
+    let b = 2;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
